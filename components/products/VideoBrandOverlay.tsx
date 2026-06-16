6c3206eedce0079bd9358f52ff0431b5043bce3f
ZiewiseSCE <kyang@ziewise.com>
2026-06-17 01:48:27 +0900

    Clean DaengDaBang hover watermark symbol

--- components/products/VideoBrandOverlay.tsx
+++ components/products/VideoBrandOverlay.tsx
@@ -2,7 +2,7 @@ export default function VideoBrandOverlay() {
     return (
         <div className="ddb-watermark-cover" aria-hidden="true">
             <div className="ddb-watermark-logo">
-                <img src="/images/logo-symbol-clean.png?v=20260617-clean" alt="" className="ddb-watermark-symbol" />
+                <img src="/images/logo-symbol.png?v=20260617-clean" alt="" className="ddb-watermark-symbol" />
                 <img src="/images/wordmark.png" alt="" className="ddb-watermark-wordmark" />
             </div>
             <div className="ddb-watermark-strip" />
